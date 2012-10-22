b9580386313f74f03e3a219823832873673844ab
Artur Rodrigues <arturhoo@gmail.com>
2012-10-22 15:46:51 -0200

    EA working properly now

--- src/individual.py
+++ src/individual.py
@@ -1,5 +1,6 @@
 import random
 from custom_io import get_graph_from_input
+from copy import deepcopy
 
 
 def mutate(i, G):
@@ -10,8 +11,8 @@ def mutate(i, G):
     while new_median in i.chromosome:
         new_median = random.choice(G.nodes())
     mutated_i = Individual()
-    to_replace = random.choice(list(i.chromosome))
-    mutated_i.chromosome = set([x if x != to_replace else new_median
+    allele_to_replace = random.choice(list(i.chromosome))
+    mutated_i.chromosome = set([x if x != allele_to_replace else new_median
                                 for x in i.chromosome])
     return mutated_i
 
@@ -39,7 +40,6 @@ def crossover(i1, i2):
 
 class Individual:
     def __init__(self, p=None, G=None):
-        self.fitness = -1
         if p is None or G is None:
             self.chromosome = set()
         else:
@@ -64,7 +64,11 @@ class Individual:
             l.append((median, d))
         return sorted(l, key=lambda tup: tup[1])
 
-    def calculate_fitness(self, G):
+    def calculate_fitness(self, G, nodes_ordered_by_demand):
+        '''calculates the fitness of the individual. The heuristic used to
+        allocate facilities do medians is to allocate the facilities with
+        greater demand first
+        '''
         self.fitness = 0
         # creates dict of median's capacities
         median_dic = {}
@@ -72,10 +76,10 @@ class Individual:
             median_dic[median] = G.node[median]['capacity']
         # assign evey node of the graph to a median,
         # including the medians themselves
-        for node, attr in G.node.iteritems():
+        for node, attr in nodes_ordered_by_demand:
             l = self.get_list_medians_ordered_by_distance(node, G)
             for median, distance in l:
-                if median_dic[median] - attr['demand'] > 0:
+                if median_dic[median] - attr['demand'] >= 0:
                     self.fitness += distance
                     median_dic[median] -= attr['demand']
                     break
@@ -84,12 +88,15 @@ class Individual:
 
 if __name__ == '__main__':
     G = get_graph_from_input('../data/SJC1.dat')
+    nodes_ordered_by_demand = sorted(G.node.items(),
+                                     key=lambda t: t[1]['demand'],
+                                     reverse=True)
     popsize = 100
     maxiter = 100
-    elite = 0.2
-    mutprob = 0.01
+    elite = 1 / popsize
+    mutprob = 0.2
     coprob = 0.6
-    tournament_size = 2
+    tournament_size = 5
     p = 10
 
     pop = []
@@ -98,7 +105,8 @@ if __name__ == '__main__':
         pop.append(individual)
 
     for i in range(maxiter):
-        scores = [(i.calculate_fitness(G), i) for i in pop]
+        scores = [(i.calculate_fitness(G, nodes_ordered_by_demand), i)
+                  for i in pop]
         scores.sort()
         ranked = [i for (s, i) in scores]
         print scores[0][0]
@@ -107,18 +115,20 @@ if __name__ == '__main__':
         new_pop = ranked[0:topelite]
         while len(new_pop) < popsize:
             random_number = random.random()
+            subpop = random.sample(pop, tournament_size)
             if random_number < mutprob:
-                c = random.randint(0, popsize - 1)
-                new_pop.append(mutate(pop[c], G))
+                new_pop.append(mutate(subpop[0], G))
             elif random_number < coprob:
-                subpop = random.sample(pop, tournament_size)
                 i1, i2 = crossover(subpop[0], subpop[1])
                 new_pop.append(i1)
                 new_pop.append(i2)
             else:
-                c = random.randint(0, popsize - 1)
-                new_pop.append(pop[c])
+                new_pop.append(deepcopy(subpop[0]))
             if len(new_pop) > popsize:
                 new_pop.pop()
 
         pop = new_pop
+
+    pop[0].apply_medians(G)
+    for node, attr in G.node.iteritems():
+        print node, attr
