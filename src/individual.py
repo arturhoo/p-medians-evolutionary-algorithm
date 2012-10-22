cec5d6b6c7c52c2a184851770053a6728a5d2882
Artur Rodrigues <arturhoo@gmail.com>
2012-10-22 16:08:55 -0200

    Individual is ready

--- src/individual.py
+++ src/individual.py
@@ -1,17 +1,15 @@
-import random
-from custom_io import get_graph_from_input
-from copy import deepcopy
+from random import choice, randint, sample
 
 
 def mutate(i, G):
     '''a random gene has its allele swapped by a random facility
     given the new facility is not present in the chromosome
     '''
-    new_median = random.choice(G.nodes())
+    new_median = choice(G.nodes())
     while new_median in i.chromosome:
-        new_median = random.choice(G.nodes())
+        new_median = choice(G.nodes())
     mutated_i = Individual()
-    allele_to_replace = random.choice(list(i.chromosome))
+    allele_to_replace = choice(list(i.chromosome))
     mutated_i.chromosome = set([x if x != allele_to_replace else new_median
                                 for x in i.chromosome])
     return mutated_i
@@ -26,10 +24,10 @@ def crossover(i1, i2):
     exchange_set1 = i1.chromosome - intersection
     exchange_set2 = i2.chromosome - intersection
 
-    try:
-        c = random.randint(1, len(exchange_set1) - 1)
-    except ValueError:  # if both individuals are the same
+    if i1.chromosome == i2.chromosome:  # if both individuals are the same
         return i1, i2
+    else:
+        c = randint(0, len(exchange_set1))
     child1, child2 = Individual(), Individual()
     child1.chromosome = set(list(exchange_set1)[0:c] +
                             list(exchange_set2)[c:]).union(intersection)
@@ -43,7 +41,7 @@ class Individual:
         if p is None or G is None:
             self.chromosome = set()
         else:
-            self.chromosome = set(random.sample(set(G.nodes()), p))
+            self.chromosome = set(sample(set(G.nodes()), p))
 
     def __repr__(self):
         return str(self.chromosome)
@@ -84,51 +82,3 @@ class Individual:
                     median_dic[median] -= attr['demand']
                     break
         return self.fitness
-
-
-if __name__ == '__main__':
-    G = get_graph_from_input('../data/SJC1.dat')
-    nodes_ordered_by_demand = sorted(G.node.items(),
-                                     key=lambda t: t[1]['demand'],
-                                     reverse=True)
-    popsize = 100
-    maxiter = 100
-    elite = 1 / popsize
-    mutprob = 0.2
-    coprob = 0.6
-    tournament_size = 5
-    p = 10
-
-    pop = []
-    for i in range(popsize):
-        individual = Individual(p, G)
-        pop.append(individual)
-
-    for i in range(maxiter):
-        scores = [(i.calculate_fitness(G, nodes_ordered_by_demand), i)
-                  for i in pop]
-        scores.sort()
-        ranked = [i for (s, i) in scores]
-        print scores[0][0]
-
-        topelite = int(elite * popsize)
-        new_pop = ranked[0:topelite]
-        while len(new_pop) < popsize:
-            random_number = random.random()
-            subpop = random.sample(pop, tournament_size)
-            if random_number < mutprob:
-                new_pop.append(mutate(subpop[0], G))
-            elif random_number < coprob:
-                i1, i2 = crossover(subpop[0], subpop[1])
-                new_pop.append(i1)
-                new_pop.append(i2)
-            else:
-                new_pop.append(deepcopy(subpop[0]))
-            if len(new_pop) > popsize:
-                new_pop.pop()
-
-        pop = new_pop
-
-    pop[0].apply_medians(G)
-    for node, attr in G.node.iteritems():
-        print node, attr
